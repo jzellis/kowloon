43638a5cda53d3bced07b298860b3d02353d782c
Joshua Ellis <jzellis@gmail.com>
2023-06-20 15:25:07 +0100

    UI and API changes

--- server/routes/get/userInbox.js
+++ server/routes/get/userInbox.js
@@ -8,10 +8,12 @@ export default async function handler(req, res, next) {
   let page = req.query.page || 1;
   if (Kowloon.user.actor && Kowloon.user.username == req.params.username) {
     let actor = `@${req.params.username}${Kowloon.settings.apDomain}`;
-    let query = { to: actor };
-    if (!req.query.mine) query["activity.actor"] = { $ne: actor };
+    let query = { to: actor, page };
+    if (req.query.type) query.types = req.query.type;
+
+    if (req.query.mine) query.showMine = true;
+    if (req.query.actors) query["activity.actor"] = { $in: actors };
     if (req.query.read) query.read = req.query.read;
-    console.log(query);
     let items = await Kowloon.getInboxItems(query, page);
     // items = items.map((i) => i.id);
     response = {

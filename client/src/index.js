45681ceff38930ec8ea0b9726031a7bfd032ce8f
Joshua Ellis <jzellis@gmail.com>
2023-07-01 12:54:36 +0100

    Lots of changes

--- client/src/index.js
+++ client/src/index.js
@@ -35,7 +35,7 @@ const router = createBrowserRouter([
     loader: loader,
   },
 ]);
-console.log(await Kowloon.login("jzellis", "***REMOVED***"));
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>

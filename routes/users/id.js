abd48a0fee706bd18e1a88846b3a968b9cae68ea
Joshua Ellis <jzellis@gmail.com>
2025-03-28 17:47:37 +0000

    Fixed user view by ID

--- routes/users/id.js
+++ routes/users/id.js
@@ -1,9 +1,13 @@
 import Kowloon from "../../Kowloon.js";
+import { User } from "../../schema/index.js";
+
 export default async function (req, res, next) {
   let status = 200;
   let qStart = Date.now();
   let query = { id: req.params.id };
-  let user = await Kowloon.getUser(query);
+  let user = await User.findOne(query).select(
+    "-flaggedAt -flaggedBy -flaggedReason -bcc -rbcc -object.bcc -object.rbcc -deletedAt -deletedBy -_id -__v -source"
+  );
   let response = {
     queryTime: Date.now() - qStart,
   };

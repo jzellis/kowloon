e2ec4e8b072ef86786090f31ac7bf2e101109f85
Joshua Ellis <jzellis@gmail.com>
2025-11-05 15:59:35 +0000

    Fixed a bad library import

--- methods/inbox/index.js
+++ methods/inbox/index.js
@@ -1,6 +1,5 @@
 import get from "./get.js";
-import parse from "./parse/index.js";
 import process from "./process.js";
 import getFeed from "./getFeed.js";
 
-export default { get, parse, process, getFeed };
+export default { get, process, getFeed };
